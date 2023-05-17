cc680c34993c33e7bcb4ca9428444646f682a9c7
Ebenezer Nikabou <nikaboue10@gmail.com>
2023-05-17 14:20:11 +0000

    Initial commit

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <PokemonListRender :pokemons="pokemons" />
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import PokemonListRender from './PokemonListRender.vue';
+
+const pokemons = ref([]);
+
+const fetchPokemons = async () => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        const data = await response.json();
+        pokemons.value = data.results;
+    } catch (error) {
+        console.error('Error fetching Pokémon data:', error);
+    }
+};
+
+onMounted(fetchPokemons);
+</script>
